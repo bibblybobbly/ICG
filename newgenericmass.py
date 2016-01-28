7d6c9c0257601786f4a89a17106257d22237d13a
bibblybobblyICG <ben.mawdsley@port.ac.uk>
2016-01-28 15:43:52 +0000

    28 jan, trying to get everything up to date with root

--- newgenericmass.py
+++ newgenericmass.py
@@ -1,64 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as mpl
-import matplotlib as matplot
+Rx=
+Ry=
 
 
-
-xsize=100
-ysize=100
-
-xpos=50
-ypos=50 #location of centre
-
-sigmax=5
-sigmay=5
-sigma=10
-fac=10000
-kons= 5*10**8 # constant in front of r^-2
-R2=np.zeros((xsize, ysize))
-theta=np.zeros((xsize, ysize))
-R=np.zeros((xsize, ysize))
-
-
-for i in range(0,xsize):#filling xsize x ysize array with gamma values 
-	for j in range(0, ysize):
-		#R[j,i]=np.exp(-((i-xpos)**2+(j-ypos)**2)/(sigma**2))
-		
-		if i-xpos ==0:
-			if j-ypos<0:
-				theta[j,i]=-(np.pi)/2
-				#R[j,i]=1/((j-ypos)**2+(i-xpos)**2)
-			if j-ypos >0:
-				theta[j,i]=(np.pi)/2
-				#R[j,i]=1/((j-ypos)**2+(i-xpos)**2)
-			if j-ypos ==0:
-				theta[j,i]=np.pi/2
-			#theta[j,i]=theta[j,i]+np.pi/2
-		else:
-			theta[j,i]=np.arctan((j-ypos+0.5)/(i-xpos+0.5))#(np.pi/2)+
-			#R[j,i]=1/((j-ypos)**2+(i-xpos)**2)
-		if j-ypos==0:
-			theta[j,i]=np.pi/4
-			
-		#print(j,i)
-		if i-xpos !=0 or j-ypos !=0:
-			R[j,i]=kons/((i-xpos+0.5)**2+(j-ypos+0.5)**2)
-		
-#R[49,50]=0.00002
-#R[50,49]=0.00002
-#R[51,50]=0.00002
-#R[50,51]=0.00002
-#R[50,50]=0
-print(np.max(theta), np.min(theta))
-
-Rx=np.multiply(R, np.cos(2*theta))
-Ry=np.multiply(R, np.sin(2*theta))
-
-mpl.quiver(Rx, Ry)
-
-mpl.show()
-
-mpl.imshow(Rx)
 
 #NEED TO DEFINE SIZES
 
@@ -79,13 +22,13 @@ gamma2t=np.fft.ifftshift(Rysft)   # converting FFT into gammas, by inverse shift
 
 gammat=gamma1t+np.multiply(gamma2t, 1j)  # gamma tilda, by definition
 
+xsize=gammat.shape[1]
+ysize=gammat.shape[0] #required length of axes
+
 #gammats=np.fft.fftshift(gammat)
 #gammas=np.fft.ifft2
 
-mpl.figure(55)
-mpl.imshow(np.real(gammat))
-print('49,49', gammat[49,49])
-print('49,51', gammat[49,51])
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 psit=np.zeros((ysize, xsize), dtype=np.complex)
@@ -109,7 +52,7 @@ for l in range(0,xsize):
 			kappat[l,i]=np.divide((xi1**2-xi2**2-2j*(xi1*xi2))*gammat[l,i], (xi1**2+xi2**2))   #calculate kappa tilda according to eqns [15/12/15]
 	
 		else:
-			kappat[l,i]=1 #is infinite at xi1=0, xi2=0
+			kappat[l,i]=1 #is infinite at xi1=0, xi2=0  
 
 	
  
@@ -134,22 +77,6 @@ psi=np.real(psi)
 kappa=np.real(kappa)
 #Produce plots of both kappa and psi
 
-#psideforeal=np.real(np.multiply(psi, np.conj(psi)))
-#kappadeforeal=np.real(np.multiply(kappa, np.conj(kappa)))
-
-#print('49,49 + 51,51', psigr 
-
-mpl.figure(5)
-mpl.subplot(1,2,1)
-mpl.imshow(psi)
-mpl.title('Real component of Psi results')
-mpl.colorbar()
-
-mpl.subplot(1,2,2)
-mpl.imshow(kappa)
-mpl.title('Real component of Kappa results')
-mpl.colorbar()
-mpl.show()
 
 
 
